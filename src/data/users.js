68da2440a688fe4c07a37105b4c87f12caade902
Leonardo Suazo Fernandezo <mf01217@gmail.com>
2026-04-11 12:15:32 -0400

    Implementa login con JWT, cookies y error 401

---
+++ src/data/users.js
@@ -0,0 +1,16 @@
+const users = [
+  {
+    id: 1,
+    username: "admin",
+    password: "1234",
+    role: "admin"
+  },
+  {
+    id: 2,
+    username: "usuario",
+    password: "abcd",
+    role: "user"
+  }
+];
+
+module.exports = users;
