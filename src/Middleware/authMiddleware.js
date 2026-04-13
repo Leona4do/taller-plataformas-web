f5144ce9f9bf4f2037b057b3435e70acd9c4a2d2
Keiner <118keiner@gmail.com>
2026-04-12 21:52:42 -0400

    Sustitución

--- src/Middleware/authMiddleware.js
+++ src/Middleware/authMiddleware.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const verificarToken = (req, res, next) => {
-    const token = req.cookies.token; // Acceso correcto a cookies
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
     if (!token) {
         return res.status(401).json({ message: "Sin autorización." });
@@ -9,7 +9,7 @@ const verificarToken = (req, res, next) => {
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        req.user = decoded; // Guardamos los datos para la ruta privada
+        req.user = decoded;
         next(); 
     } catch (error) {
         res.status(401).json({ message: "Token inválido o expirado" });
